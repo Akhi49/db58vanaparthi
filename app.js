79b2937a553e09ccc911e973a73c54d6a1d4ec30
Akhil Vanaparthi <vanaparthiakhil3@gmail.com>
2021-11-12 12:36:39 -0600

    updated all files

--- app.js
+++ app.js
@@ -3,12 +3,56 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var Costume = require("./models/costume"); 
+
+const connectionString = process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
+db.once("open", function(){
+  console.log("Connection to DB succeeded")
+});
+
+// We can seed the collection if needed on server start 
+async function recreateDB(){ 
+  // Delete everything 
+  await Costume.deleteMany(); 
+ 
+  let instance1 = new Costume({costume_type:"car",  size:'camaron', cost:1000});
+  let instance2 = new Costume({costume_type:"card",  size:'uno', cost:25.4});
+  let instance3 = new Costume({costume_type:"benz",  size:'911', cost:70000});
+
+  instance1.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("First object saved") 
+  }); 
+
+  instance2.save( function(err,doc) { 
+    if(err) return console.error(err); 
+    console.log("Second object saved") 
+}); 
+
+  instance3.save( function(err,doc) { 
+    if(err) return console.error(err); 
+    console.log("Third object saved") 
+}); 
+} 
+ 
+let reseed = true; 
+if (reseed) { recreateDB();} 
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var carsRouter = require('./routes/cars');
-var addmodsRouter = require('./routes/addmods');
+var addModsRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
+var resourceRouter = require('./routes/resource');
 
 var app = express();
 
@@ -24,12 +68,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/cars', carsRouter);
-app.use('/addmods', addmodsRouter);
+app.use('/addmods', addModsRouter);
 app.use('/selector', selectorRouter);
-
-
-
+app.use('/resource', resourceRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
