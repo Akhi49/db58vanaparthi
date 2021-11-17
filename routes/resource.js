4cfc499390ff060b80182d5e630e537eb9301826
Akhil Vanaparthi <vanaparthiakhil3@gmail.com>
2021-11-17 14:06:46 -0600

    updated

--- routes/resource.js
+++ routes/resource.js
@@ -15,20 +15,20 @@ router.get('/', api_controller.api);
 /// COSTUME ROUTES /// 
  
 // POST request for creating a Costume.  
-router.post('/costumes', costume_controller.costume_create_post); 
+router.post('/costume', costume_controller.costume_create_post); 
  
 // DELETE request to delete Costume. 
-router.delete('/costumes/:id', costume_controller.costume_delete); 
+router.delete('/costume/:id', costume_controller.costume_delete); 
  
 // PUT request to update Costume. 
-router.put('/costumes/:id', 
+router.put('/costume/:id', 
 costume_controller.costume_update_put); 
  
 // GET request for one Costume. 
-router.get('/costumes/:id', costume_controller.costume_detail); 
+router.get('/costume/:id', costume_controller.costume_detail); 
  
 // GET request for list of all Costume items. 
-router.get('/costumes', costume_controller.costume_list); 
+router.get('/costume', costume_controller.costume_list); 
 
 router.get('/', costume_controlers.costume_view_all_Page ); 
  
